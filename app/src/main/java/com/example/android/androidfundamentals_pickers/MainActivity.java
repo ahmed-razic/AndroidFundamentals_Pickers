b08b67bf1e16c4a9c247cd23fa9f24e71e7d7eaf
ahmed-razic <ahmed.razic@gmail.com>
2021-02-10 13:54:33 +0100

    finish

--- app/src/main/java/com/example/android/androidfundamentals_pickers/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_pickers/MainActivity.java
@@ -10,23 +10,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Creates the view.
+     * @param savedInstanceState Saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Handles the button click to create a new date picker fragment and
+     * show it.
+     *
+     * @param view View that was clicked
+     */
     public void showDatePicker(View view) {
         DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(),"datePicker");
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.datepicker));
     }
 
+    /**
+     * Process the date picker result into strings that can be displayed in
+     * a Toast.
+     *
+     * @param year Chosen year
+     * @param month Chosen month
+     * @param day Chosen day
+     */
     public void processDatePickerResult(int year, int month, int day) {
-        String month_string = Integer.toString(month+1);
+        String month_string = Integer.toString(month + 1);
         String day_string = Integer.toString(day);
         String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        String dateMessage = (day_string +
+                "/" + month_string +
+                "/" + year_string);
 
-        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
